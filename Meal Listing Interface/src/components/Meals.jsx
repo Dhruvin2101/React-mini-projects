c1e79b4f58af75d2d7fed53b9e04892a19a4056b
dhruvincodes21 <dhruvin.codes@gmail.com>
2026-05-05 15:48:09 +0530

    made all the links work and the eye animate on hover

--- Meal Listing Interface/src/components/Meals.jsx
+++ Meal Listing Interface/src/components/Meals.jsx
@@ -14,17 +14,29 @@ const Meals = ({ data }) => {
             <div className="name">{data.strMeal}</div>
           </div>
           <div className="Area">Type: {data.strArea}</div>
-          <div className="Ingredients">Ingredient:
+          <div className="Ingredients">
+            Ingredient:
             <div className="ingredient-list">
-            <div className="ingredient-box">{data.strIngredient1}</div>
-            <div className="ingredient-box">{data.strIngredient2}</div>
-            <div className="ingredient-box">{data.strIngredient3}</div>
+              <div className="ingredient-box">{data.strIngredient1}</div>
+              <div className="ingredient-box">{data.strIngredient2}</div>
+              <div className="ingredient-box">{data.strIngredient3}</div>
             </div>
           </div>
         </div>
         <div className="button-container">
-          <button className="btn">More Info</button>
-          <button className="btn">Preparation</button>
+          <button
+            className="btn"
+            onClick={() => {
+              window.open(data.strSource, "_blank");
+            }}
+          >
+            More Info
+          </button>
+          <button className="btn" onClick={() => {
+              window.open(data.strYoutube, "_blank");
+            }}>
+            Preparation
+          </button>
         </div>
       </div>
     </>
